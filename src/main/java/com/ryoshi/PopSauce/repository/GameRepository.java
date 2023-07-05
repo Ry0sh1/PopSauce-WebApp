5d24b20b64138d8e646b5b559e6f1121ebcbcf35
Tim Lagier <tim.lagier33@gmail.com>
2023-07-05 17:32:51 +0200

    Test
    Test

--- src/main/java/com/ryoshi/PopSauce/repository/GameRepository.java
+++ src/main/java/com/ryoshi/PopSauce/repository/GameRepository.java
@@ -4,6 +4,9 @@ import com.ryoshi.PopSauce.entity.Game;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GameRepository extends CrudRepository<Game, Long> {
+    public List<Game> findAllByCode (String code);
 }
