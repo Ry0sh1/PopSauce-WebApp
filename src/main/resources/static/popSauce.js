3e0acc92a347d1d4d4cb44564ac1c746ceafb301
Ryoshi <fischerklon3@gmail.com>
2023-07-07 03:42:52 +0200

    The Game is now completely working. I added a start button to start the game. When you start the game, everything gets visible. You have 15 seconds to guess where the pictures are from. If you guess right you get 10 points. After 15 seconds the result gets showed for 5 seconds and then the next picture shuffles in.

--- src/main/resources/static/popSauce.js
+++ src/main/resources/static/popSauce.js
@@ -1,12 +1,45 @@
 let pictures = [];
 let currentIndex = 0;
-const h1Element = document.getElementById('anime')
+const h1Element = document.getElementById('anime');
+const timerElement = document.getElementById('timer');
+
+//Playground
+const inputElement = document.getElementById('input');
+const inputLabelElement = document.getElementById('input-label');
+const goButton = document.getElementById('go');
+const pointElement = document.getElementById('points');
+
+//Settings
+const seconds = 15;
+const resultTime = 5;
+
+function start(){
+    //Show Playground
+    inputElement.classList.remove('invisible');
+    inputLabelElement.classList.remove('invisible');
+    goButton.classList.remove('invisible');
+    pointElement.classList.remove('invisible');
+
+    //Remove Start Button
+    document.getElementById('start-button').remove();
+
+    //Start Showing Picture
+    nextPicture();
+    let timer = setInterval(changeTime,1000);
+    let getNextPictures = setInterval(nextPicture,1000*(seconds+resultTime));
+}
+
+function showResult(){
+    h1Element.innerText = pictures[currentIndex].rightGuess.split(',')[0];
+}
+
+function hideResult(){
+    h1Element.innerText = '';
+}
 
 function displayCurrentPicture() {
     let container = document.getElementById('picture-here');
     container.innerHTML = '';  // Clear existing content
-
-    h1Element.innerText = pictures[currentIndex].rightGuess;
 
     let buffer = pictures[currentIndex].content;
     let uint8Array = new Uint8Array(buffer);
@@ -28,13 +61,14 @@ function fetchPictures() {
                 return;
             }
             console.log(pictures.length);
+            document.getElementById('start-button').classList.remove('invisible');
         })
         .catch(error => {
             console.log('Fehler beim Abrufen der Bilder:', error);
         });
 }
 
-document.getElementById('getPicture').addEventListener('click', function() {
+function nextPicture() {
     if (pictures.length === 0) {
         console.log('No pictures available.');
         return;
@@ -42,5 +76,39 @@ document.getElementById('getPicture').addEventListener('click', function() {
     currentIndex = (currentIndex + 1) % pictures.length;
     console.log(pictures.length);
     console.log(currentIndex);
+    goButton.classList.remove('invisible');
+    inputLabelElement.classList.remove('right-answer');
+    hideResult();
+    setTime(seconds);
     displayCurrentPicture();
-});
+}
+
+function changeTime(time){
+    if (parseInt(getTime())===0){
+        showResult();
+        setTime(resultTime);
+    }
+    timerElement.innerText = parseInt(getTime())-1;
+}
+
+function getTime(){
+    return timerElement.innerText;
+}
+
+function setTime(time){
+    timerElement.innerText = time;
+}
+
+function rightAnswer(){
+    let result = pictures[currentIndex].rightGuess.toLowerCase().split(',');
+    let input = inputElement.value;
+    inputElement.value = '';
+    for (let i = 0; i < result.length;i++){
+        if (input === result[i]){
+            pointElement.innerText = (parseInt(pointElement.innerText) + 10);
+            inputLabelElement.classList.add('right-answer');
+            goButton.classList.add('invisible');
+        }
+    }
+}
+fetchPictures();
